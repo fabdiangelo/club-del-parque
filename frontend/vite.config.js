fa565839ab95daf3c42df4ca275ed702ca5f8e3a
Alan Fránquez <santifranquez@gmail.com>
2025-10-31 23:45:03 -0300

    pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 
 const PROJECT = 'club-del-parque-68530'
 const REGION  = 'us-central1'  
 const FUNC    = 'api'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+      registerType: 'autoUpdate',
+      manifest: require('./public/manifest.json'),
+      devOptions: {
+        enabled: true
+      }
+    })],
   server: {
     proxy: {
       '/api': {
